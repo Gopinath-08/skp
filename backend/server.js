e84b749c168ddd022234e0ddc6172fe1ac0d0b63
gopinath-08 <gmajhi129@gmail.com>
2026-05-12 03:23:41 +0530

    just fix

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // Static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use(express.static(path.join(__dirname, '../frontend/views')));
 
 // Routes
 const authRoutes = require('./routes/auth');
